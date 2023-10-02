0722436637e8ade1bfa6da8ca02ad1d261b3d3d3
Michael <michael.leshowitz@gmail.com>
2023-10-01 22:24:37 -0400

    added dev tools

--- react-app/entrypoint.d/02-install-react-app.sh
+++ react-app/entrypoint.d/02-install-react-app.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 set -e
 
 echo "installing react-app"
--- react-app/entrypoint.d/03-install-dev-tools.sh
+++ react-app/entrypoint.d/03-install-dev-tools.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 set -e
 
 echo "installing dev tools"
 
-cd /home/react-app/
+# cd /home/react-app/
 
 npm install -g json-server
+npm install -g cmd-env
 
 echo "Done installing dev tools"
